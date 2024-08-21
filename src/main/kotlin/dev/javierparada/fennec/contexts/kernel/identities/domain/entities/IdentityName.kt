f0c7f8fe2697caec7df9fd61f87ed5a6f26b0544
Javier Parada <jpt73@pm.me>
2024-08-21 16:19:07 +0200

    Add name

---
+++ src/main/kotlin/dev/javierparada/fennec/contexts/kernel/identities/domain/entities/IdentityName.kt
@@ -0,0 +1,18 @@
+package dev.javierparada.fennec.contexts.kernel.identities.domain.entities
+
+import dev.javierparada.fennec.contexts.shared.errors.domain.InvalidValueError
+
+data class IdentityName(
+    private val value: String,
+) {
+
+    init {
+        if (value.isEmpty() || value.isBlank()) {
+            throw InvalidValueError("Identity name cannot be empty or null")
+        }
+    }
+
+    fun value(): String {
+        return this.value
+    }
+}
