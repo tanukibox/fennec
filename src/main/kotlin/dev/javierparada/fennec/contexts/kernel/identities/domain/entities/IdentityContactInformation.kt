d68a3989d7bb897d7a7fde611e39e145d749ca16
Javier Parada <jpt73@pm.me>
2024-08-21 16:43:48 +0200

    Add contact information

---
+++ src/main/kotlin/dev/javierparada/fennec/contexts/kernel/identities/domain/entities/IdentityContactInformation.kt
@@ -0,0 +1,18 @@
+package dev.javierparada.fennec.contexts.kernel.identities.domain.entities
+
+import dev.javierparada.fennec.contexts.shared.errors.domain.InvalidValueError
+
+data class IdentityContactInformation(
+    private val value: String?,
+) {
+
+    init {
+        if (value != null && value.isBlank()) {
+            throw InvalidValueError("Identity description cannot be empty")
+        }
+    }
+
+    fun value(): String? {
+        return this.value
+    }
+}
