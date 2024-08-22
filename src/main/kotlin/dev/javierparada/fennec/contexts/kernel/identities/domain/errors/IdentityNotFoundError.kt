d0b32ed375041101a95c37b6b235daf9f97fa913
Javier Parada <jpt73@pm.me>
2024-08-22 11:10:35 +0200

    add identity repository

---
+++ src/main/kotlin/dev/javierparada/fennec/contexts/kernel/identities/domain/errors/IdentityNotFoundError.kt
@@ -0,0 +1,4 @@
+package dev.javierparada.fennec.contexts.kernel.identities.domain.errors
+
+class IdentityNotFoundError {
+}
--- src/main/kotlin/dev/javierparada/fennec/contexts/kernel/identities/domain/repositories/IdentityRepository.kt
+++ src/main/kotlin/dev/javierparada/fennec/contexts/kernel/identities/domain/repositories/IdentityRepository.kt
@@ -1,4 +1,16 @@
 package dev.javierparada.fennec.contexts.kernel.identities.domain.repositories
 
+import dev.javierparada.fennec.contexts.kernel.identities.domain.entities.Identity
+import dev.javierparada.fennec.contexts.kernel.shared.domain.entities.node.FennecNodeId
+
 interface IdentityRepository {
+
+    fun findById(id: FennecNodeId): Identity
+
+    fun createOne(identity: Identity)
+
+    fun updateOne(identity: Identity)
+
+    fun deleteOne(id: FennecNodeId)
+
 }
