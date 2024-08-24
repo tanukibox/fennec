d98c54e435155aa0984792d41b4c986162ffaf2c
Javier Parada <jpt73@pm.me>
2024-08-24 13:55:55 +0200

    command and query buses

--- src/main/kotlin/dev/javierparada/fennec/contexts/shared/cqrs/domain/CommandBus.kt
+++ src/main/kotlin/dev/javierparada/fennec/contexts/shared/cqrs/domain/CommandBus.kt
@@ -1,4 +1,9 @@
 package dev.javierparada.fennec.contexts.shared.cqrs.domain
 
 interface CommandBus {
+
+    fun dispatch(command: Command)
+
+    fun subscribe(command: CommandHandler)
+
 }
--- src/main/kotlin/dev/javierparada/fennec/contexts/shared/cqrs/domain/QueryBus.kt
+++ src/main/kotlin/dev/javierparada/fennec/contexts/shared/cqrs/domain/QueryBus.kt
@@ -1,4 +1,9 @@
 package dev.javierparada.fennec.contexts.shared.cqrs.domain
 
 interface QueryBus {
+
+    fun ask(query: Query)
+
+    fun subscribe(handler: QueryHandler)
+
 }
