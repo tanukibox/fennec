872ccc55a9321cb8baed0144fd8506ee7f34b6f8
Javier Parada <jpt73@pm.me>
2024-08-24 13:55:11 +0200

    add query and command handlers

--- src/main/kotlin/dev/javierparada/fennec/contexts/shared/cqrs/domain/CommandHandler.kt
+++ src/main/kotlin/dev/javierparada/fennec/contexts/shared/cqrs/domain/CommandHandler.kt
@@ -1,4 +1,9 @@
 package dev.javierparada.fennec.contexts.shared.cqrs.domain
 
 interface CommandHandler {
+
+    fun handle(command: Command)
+
+    fun getSubscriptions(): Set<String>
+
 }
--- src/main/kotlin/dev/javierparada/fennec/contexts/shared/cqrs/domain/QueryHandler.kt
+++ src/main/kotlin/dev/javierparada/fennec/contexts/shared/cqrs/domain/QueryHandler.kt
@@ -1,4 +1,9 @@
 package dev.javierparada.fennec.contexts.shared.cqrs.domain
 
 interface QueryHandler {
+
+    fun handle(query: Query)
+
+    fun getSubscriptions(): Set<String>
+
 }
